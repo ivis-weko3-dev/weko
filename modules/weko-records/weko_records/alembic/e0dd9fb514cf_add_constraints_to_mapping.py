c8f2a6d162b4d97101b8a15446c50c1777e25e4d
ivis-kuroda <tomohiro.kuroda@ivis.co.jp>
2025-09-03 18:57:56 +0900

    add: alembic for item_type_mapping.

---
+++ modules/weko-records/weko_records/alembic/e0dd9fb514cf_add_constraints_to_mapping.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 National Institute of Informatics.
+#
+# WEKO-Records is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Add constraints to mapping"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e0dd9fb514cf'
+down_revision = 'e3b07ec6e628'
+branch_labels = ()
+depends_on = None
+
+
+def upgrade():
+    """Upgrade database."""
+    with op.batch_alter_table('item_type_mapping') as batch_op:
+        batch_op.create_unique_constraint(
+            'uq_item_type_mapping_item_type_id', ['item_type_id']
+        )
+        batch_op.create_foreign_key(
+            'fk_item_type_mapping_item_type_id_item_type',
+            'item_type', ['item_type_id'], ['id']
+        )
+
+
+def downgrade():
+    """Downgrade database."""
+    with op.batch_alter_table('item_type_mapping') as batch_op:
+        batch_op.drop_constraint(
+            'fk_item_type_mapping_item_type_id_item_type', type_='foreignkey'
+        )
+        batch_op.drop_constraint(
+            'uq_item_type_mapping_item_type_id', type_='unique'
+        )
