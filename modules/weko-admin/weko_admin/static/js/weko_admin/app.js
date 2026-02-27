90886ae69ae1aebe9e11be6a36e6bda17f7a2127
ivis-kikuchi <shunya.kikuchi@ivis.co.jp>
2026-02-27 17:04:58 +0900

    fix

--- modules/weko-admin/weko_admin/static/js/weko_admin/app.js
+++ modules/weko-admin/weko_admin/static/js/weko_admin/app.js
@@ -28,9 +28,12 @@
       $scope.fetch=function(result){
         $scope.dbJson = angular.fromJson(result.slice(2,-2).replace(/\n/g,'\\n'));
         console.log($scope.dbJson);
-        for(var data in $scope.dbJson.site_license){
-          var b = {ipCheckFlg:false,ipRangeCheck:false};
-          $scope.ipCheckFlgArry.push(b);
+        for(var i=0;i < $scope.dbJson.site_license.length;i++){
+          $scope.ipCheckFlgArry[i] =[];
+          for(var j=0;j < $scope.dbJson.site_license[i].addresses.length; j++){
+            var b = {ipCheckFlg:false,ipRangeCheck:false};
+            $scope.ipCheckFlgArry[i][j]=b;
+          }
         }
       };
 
@@ -54,10 +57,13 @@
       $scope.addNewRowRange = function(ipIndex) {
          var ipAddressRange = {start_ip_address:[],finish_ip_address:[]};
          $scope.dbJson.site_license[ipIndex].addresses.push(ipAddressRange);
+         var subCheckFlg = {ipCheckFlg:false,ipRangeCheck:false};
+         $scope.ipCheckFlgArry[ipIndex].push(subCheckFlg);
       }
 
-      $scope.removeipaddress = function(ipIndex,index) {
+      $scope.removeipaddress = function(ipIndex,index,index2) {
         index.addresses.splice(ipIndex,1)
+        $scope.ipCheckFlgArry[index2].splice(ipIndex,1)
       }
       
       //add a new site License
@@ -72,8 +78,7 @@
         $scope.dbJson.site_license.push(siteLicenseJson);
 
         var subCheckFlg = {ipCheckFlg:false,ipRangeCheck:false};
-        $scope.ipCheckFlgArry.push(subCheckFlg);
-
+        $scope.ipCheckFlgArry.push([subCheckFlg]);
       }
       // delete selected site License
       $scope.deleteSiteLicense = function(ipIndex){
@@ -104,33 +109,15 @@
       }
       //commit
       $scope.commitData=function(){
-        var dbjosn = $scope.dbJson;
-        for(var chk1 in dbjosn.site_license){
-           for(var chk2 in dbjosn.site_license[chk1].addresses){
-             var saddr = "";
-             var faddr = "";
-             for(var i=0; i<4; i++){
-                tmp_s=dbjosn.site_license[chk1].addresses[chk2].start_ip_address[i];
-                if (typeof tmp_s!=='undefined' && tmp_s.length > 0) {
-                  saddr += ("00" + tmp_s).slice(-3);
-                }
-                tmp_f=dbjosn.site_license[chk1].addresses[chk2].finish_ip_address[i]
-                if (typeof tmp_f!=='undefined' && tmp_f.length > 0) {
-                  faddr += ("00" + tmp_f).slice(-3);
-                }
-             }
-
-             if (!saddr || !faddr){
-                $scope.ipCheckFlgArry[chk1].ipCheckFlg = true;
-             }
-             else if (parseInt(saddr) > parseInt(faddr)){
-                $scope.ipCheckFlgArry[chk1].ipRangeCheck = true;
-                return;
-             }
-           }
-           $scope.ipCheckFlgArry[chk1].ipRangeCheck = false;
+        rangecheck($scope);
+        isError = false
+        for(var i=0;i < $scope.dbJson.site_license.length;i++){
+          for(var j=0;j < $scope.dbJson.site_license[i].addresses.length; j++){
+            if($scope.ipCheckFlgArry[i][j].ipCheckFlg === true || $scope.ipCheckFlgArry[i][j].ipRangeCheck === true ){
+                isError = true
+            }
+          }
         }
-        isError = $scope.ipCheckFlgArry.filter(function(item){ return item.ipCheckFlg === true || item.ipRangeCheck === true })[0];
         if (!isError) {
           var url = $location.path();
           dbJson = $scope.dbJson;
@@ -144,14 +131,16 @@
       }
 
       //入力チェック
-      $scope.chcckStr=function(str,p_index){
+      $scope.chcckStr=function(str,p_index,index){
         var checkStr1 = /^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$/; //正整数
         var flg = checkStr1.test(str);
         if(!flg){
-          $scope.ipCheckFlgArry[p_index].ipCheckFlg = true;
+          $scope.ipCheckFlgArry[p_index][index].ipCheckFlg = true;
+          return
         }else{
-          $scope.ipCheckFlgArry[p_index].ipCheckFlg = false;
+          $scope.ipCheckFlgArry[p_index][index].ipCheckFlg = false;
         }
+        rangecheck($scope)
       }
     }
     // Inject depedencies
@@ -175,3 +164,33 @@
       document.getElementById('siteLicense'), ['siteLicenseModule']);
   });
 })(angular);
+
+function rangecheck($scope){
+   var dbjosn = $scope.dbJson;
+   for(var chk1 in dbjosn.site_license){
+        for(var chk2 in dbjosn.site_license[chk1].addresses){
+             var saddr = "";
+             var faddr = "";
+             for(var i=0; i<4; i++){
+                tmp_s=dbjosn.site_license[chk1].addresses[chk2].start_ip_address[i];
+                if (typeof tmp_s!=='undefined' && tmp_s.length > 0) {
+                  saddr += ("00" + tmp_s).slice(-3);
+                }
+                tmp_f=dbjosn.site_license[chk1].addresses[chk2].finish_ip_address[i]
+                if (typeof tmp_f!=='undefined' && tmp_f.length > 0) {
+                  faddr += ("00" + tmp_f).slice(-3);
+                }
+             }
+             
+             if (!saddr || !faddr){
+                $scope.ipCheckFlgArry[chk1][chk2].ipCheckFlg = true;
+             }
+             else if (parseInt(saddr) > parseInt(faddr)){
+                $scope.ipCheckFlgArry[chk1][chk2].ipRangeCheck = true;
+             }
+             else{
+                $scope.ipCheckFlgArry[chk1][chk2].ipRangeCheck = false;
+             }
+           }
+        }
+}
