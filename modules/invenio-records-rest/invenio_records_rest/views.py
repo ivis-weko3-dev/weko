4c7982d2463b4ee6a71bd7cf8074553d5e64ad92
ivis-kikuchi <shunya.kikuchi@ivis.co.jp>
2026-03-26 09:20:11 +0900

    fix search

--- modules/invenio-records-rest/invenio_records_rest/views.py
+++ modules/invenio-records-rest/invenio_records_rest/views.py
@@ -827,9 +827,7 @@ class RecordsListResource(ContentNegotiatedMethodView):
             urlkwargs['q'] = query
 
         if is_custom_sort :
-            self._override_params_for_customsort(
-                search, is_asc
-            )
+            search = self._override_params_for_customsort(search, is_asc)
         search_result = search.execute()
         search_result_dict = search_result.to_dict()
 
@@ -918,7 +916,7 @@ class RecordsListResource(ContentNegotiatedMethodView):
 
         search._sort.append(path_sort)
         search._sort.append(default_sort)
-
+        return search
 
     @classmethod
     def _do_custom_sort(cls, search_result_dict, target_index, is_asc, page, size):
