8708ff75955b7dda77316dda22e961be576d7522
ayumi-nishida <ayumi.nishida@za.ivis.co.jp>
2026-04-08 09:54:44 +0900

    nginxコンテナのログを標準出力できるように修正

--- nginx/startup.sh
+++ nginx/startup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-pm2="pm2 start /usr/local/ecosystem.config.js"
+pm2="pm2 start /usr/local/ecosystem.config.js -s"
 eval $pm2
+
+tail -F /root/.pm2/logs/AMS-out.log >> /dev/stdout 2> >(grep -v "No such file or directory" >&2) &
+tail -F /root/.pm2/logs/AMS-error.log >> /dev/stderr 2> >(grep -v "No such file or directory" >&2) &
 
 supervisor="/usr/bin/supervisord -c /etc/supervisor/supervisord.conf"
 eval $supervisor
