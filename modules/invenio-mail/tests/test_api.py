ed695aac0da9bc1360feb1544f75bf13279e67d5
kaho.kondo <kaho.kondo@ivis.co.jp>
2026-07-15 18:59:33 +0900

    feat(invenio-mail): add DomainConnection tests

--- modules/invenio-mail/tests/test_api.py
+++ modules/invenio-mail/tests/test_api.py
@@ -8,7 +8,7 @@
 
 """Test package API."""
 
-from invenio_mail.api import TemplatedMessage, send_mail
+from invenio_mail.api import TemplatedMessage, DomainConnection, send_mail
 from unittest.mock import patch
 from smtplib import SMTPServerDisconnected
 
@@ -52,3 +52,94 @@ def test_send_mail(app, mail_configs, mocker):
     msg = args[0]
     assert msg.subject == "test_subject"
     assert msg.html == "test_html"
+
+
+class _FakeMail:
+    """Connection.configure_host が参照する属性を持つダミーのMailオブジェクト。"""
+
+    def __init__(self, server="mail.example.com", port=25, use_ssl=False,
+                 use_tls=False, local_hostname=None, debug=0,
+                 username=None, password=None):
+        self.server = server
+        self.port = port
+        self.use_ssl = use_ssl
+        self.use_tls = use_tls
+        self.local_hostname = local_hostname
+        self.debug = debug
+        self.username = username
+        self.password = password
+
+
+class TestDomainConnection:
+
+    # .tox/c1/bin/pytest --cov=invenio_mail tests/test_api.py::TestDomainConnection::test_configure_host -vv -s --cov-branch --cov-report=term --basetemp=/code/modules/invenio-mail/.tox/c1/tmp
+    def test_configure_host(self, mocker):
+        # use_ssl=True, local_hostname="myhost" -> SMTP_SSLにlocal_hostnameを渡す
+        mock_host = mocker.Mock()
+        mock_ssl = mocker.patch(
+            "invenio_mail.api.smtplib.SMTP_SSL", return_value=mock_host)
+        mail = _FakeMail(server="mail.example.com", port=465,
+                          use_ssl=True, local_hostname="myhost")
+        conn = DomainConnection(mail)
+        host = conn.configure_host()
+        mock_ssl.assert_called_once_with(
+            "mail.example.com", 465, local_hostname="myhost")
+        assert host is mock_host
+        mock_host.set_debuglevel.assert_called_once_with(0)
+        mock_host.starttls.assert_not_called()
+        mock_host.login.assert_not_called()
+
+        # use_ssl=True, local_hostname=None -> local_hostnameを渡さない
+        mock_host = mocker.Mock()
+        mock_ssl = mocker.patch(
+            "invenio_mail.api.smtplib.SMTP_SSL", return_value=mock_host)
+        mail = _FakeMail(use_ssl=True, local_hostname=None)
+        conn = DomainConnection(mail)
+        host = conn.configure_host()
+        mock_ssl.assert_called_once_with(mail.server, mail.port)
+        assert host is mock_host
+
+        # use_ssl=False, local_hostname="test.example.com" -> SMTPにlocal_hostnameを渡す
+        mock_host = mocker.Mock()
+        mock_smtp = mocker.patch(
+            "invenio_mail.api.smtplib.SMTP", return_value=mock_host)
+        mail = _FakeMail(use_ssl=False, local_hostname="test.example.com")
+        conn = DomainConnection(mail)
+        host = conn.configure_host()
+        mock_smtp.assert_called_once_with(
+            mail.server, mail.port, local_hostname="test.example.com")
+        assert host is mock_host
+
+        # use_ssl=False, local_hostname=None -> local_hostnameを渡さない
+        mock_host = mocker.Mock()
+        mock_smtp = mocker.patch(
+            "invenio_mail.api.smtplib.SMTP", return_value=mock_host)
+        mail = _FakeMail(use_ssl=False, local_hostname=None)
+        conn = DomainConnection(mail)
+        host = conn.configure_host()
+        mock_smtp.assert_called_once_with(mail.server, mail.port)
+        assert host is mock_host
+
+        # use_tls=True -> starttls()が呼ばれる
+        mock_host = mocker.Mock()
+        mocker.patch("invenio_mail.api.smtplib.SMTP", return_value=mock_host)
+        mail = _FakeMail(use_tls=True)
+        conn = DomainConnection(mail)
+        conn.configure_host()
+        mock_host.starttls.assert_called_once()
+
+        # username/passwordが設定されている場合 -> login()が呼ばれる
+        mock_host = mocker.Mock()
+        mocker.patch("invenio_mail.api.smtplib.SMTP", return_value=mock_host)
+        mail = _FakeMail(username="user", password="pass")
+        conn = DomainConnection(mail)
+        conn.configure_host()
+        mock_host.login.assert_called_once_with("user", "pass")
+
+        # debugの値がintに変換されset_debuglevelに渡る
+        mock_host = mocker.Mock()
+        mocker.patch("invenio_mail.api.smtplib.SMTP", return_value=mock_host)
+        mail = _FakeMail(debug=True)
+        conn = DomainConnection(mail)
+        conn.configure_host()
+        mock_host.set_debuglevel.assert_called_once_with(1)
