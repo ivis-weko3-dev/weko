8a2e9a7b34a278c9f8df0518d9a6bec9bcde2d6b
ivis-miyachi <kyosuke.miyachi@ivis.co.jp>
2026-09-02 20:39:40 +0900

    Fixed the placement of JS and CSS files.(#62719)

--- modules/weko-items-ui/weko_items_ui/assets/bootstrap3/js/weko_items_ui/item.autofill.js
+++ modules/weko-items-ui/weko_items_ui/assets/bootstrap3/js/weko_items_ui/item.autofill.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 class ModalHeader extends React.Component {
     render() {
         return (
--- modules/weko-records-ui/weko_records_ui/assets/bootstrap3/js/weko_records_ui/pidstore_identifier.js
+++ modules/weko-records-ui/weko_records_ui/assets/bootstrap3/js/weko_records_ui/pidstore_identifier.js
@@ -19,7 +19,7 @@
  * Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
  * MA 02111-1307, USA.
  */
-
+import $ from 'jquery';
 $(document).ready(function () {
   const leftSelect = $('#leftSelect');
   const rightSelect = $('#rightSelect');
